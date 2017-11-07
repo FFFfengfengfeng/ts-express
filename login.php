ef033e4214d6e8bab55a98d80a90f62c512246e8
FFFfengfengfeng <1098865109@qq.com>
2017-11-07 19:30:26 +0800

    修改登录页

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>欢迎登录</title>
+</head>
+<body>
+    <div class="login-box">
+        <h3>欢迎登录</h3>
+    </div>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
